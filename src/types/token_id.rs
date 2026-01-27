532a40d12e2279ffad71acdcbe42be43900883a7
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2026-01-28 06:57:17 +0700

    fix: cache_download_command

--- src/types/token_id.rs
+++ src/types/token_id.rs
@@ -3,10 +3,6 @@ use serde::Serializer;
 
 pub type TokenId = U256;
 
-pub fn to_fjall_key_from_token_id(token_id: TokenId) -> [u8; 32] {
-    token_id.to_be_bytes::<32>()
-}
-
 pub fn serialize_as_decimal<S: Serializer>(token_id: &TokenId, serializer: S) -> Result<S::Ok, S::Error> {
     let decimal_string = token_id.to_string();
     serializer.serialize_str(&decimal_string)
